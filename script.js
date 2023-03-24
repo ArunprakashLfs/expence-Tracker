e857554568ee1ba1db07aa7c27e442c40f9de6dc
kesavan <arunprakash7871@gmail.com>
2023-03-24 01:37:40 -0400

    getting input from html

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-''
+'use strict'
+//getting input
+let inputEl = document.getElementById("input");
+let valueEl = document.getElementById("number");
+let submitEl = document.getElementById("submit");
