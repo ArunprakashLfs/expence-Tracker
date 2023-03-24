d7c0f3c359c523701abe4beb6d4375570b2ef36c
kesavan <arunprakash7871@gmail.com>
2023-03-24 01:41:02 -0400

    add event listners

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 let inputEl = document.getElementById("input");
 let valueEl = document.getElementById("number");
 let submitEl = document.getElementById("submit");
+
+//functions
+
+//EventListners
+submitEl.addEventListener("click", function(e){
+    e.preventDefault();
+    let input = inputEl.value;
+    let expence = valueEl.value;
+    console.log(input, expence);
+    
+})
